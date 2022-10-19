4b99d34493a7768e53d208f45185e9a2ca5d01b0
ntieng <noveltieng@gmail.com>
2022-10-19 12:19:33 +0800

    initial-commit

---
+++ Kafka-Sample/WeatherConsumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+// Declare topic
+string topic = "weather-topic";
+
+// Declare producer config
+// localhost:9092 is broker
+var config = new ConsumerConfig
+{
+    GroupId = "weather-consumer-group",
+    BootstrapServers = "localhost:9092",
+    AutoOffsetReset = AutoOffsetReset.Earliest
+};
+
+// Build new consumer
+using var consumer = new ConsumerBuilder<Null, string>(config).Build();
+
+// Subscribe to topic
+consumer.Subscribe(topic);
+
+CancellationTokenSource token = new();
+try
+{
+    while (true)
+    {
+        var response = consumer.Consume(token.Token);
+        if (response.Message != null)
+        {
+            // Receive message
+            var weather = JsonConvert.DeserializeObject<Weather>(response.Message.Value);
+            Console.WriteLine($"State: {weather.State}, " + $"Temp: {weather.Temperature}F");
+        }
+    }
+}
+catch (ConsumeException ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public record Weather(string State, int Temperature);
---
+++ Kafka-Sample/WeatherProducer/Program.cs
@@ -0,0 +1,29 @@
+﻿using Confluent.Kafka;
+using Newtonsoft.Json;
+
+// Declare topic
+string topic = "weather-topic";
+
+// Declare producer config
+// localhost:9092 is broker
+var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+// Build new producer
+using var producer = new ProducerBuilder<Null, string>(config).Build();
+
+try
+{
+    string? state;
+    while ((state = Console.ReadLine()) != null)
+    {
+        // Send message
+        var response = await producer.ProduceAsync(topic, new Message<Null, string> { Value = JsonConvert.SerializeObject(new Weather(state, 70)) });
+        Console.WriteLine(response.Value);
+    }
+}
+catch (ProduceException<Null, string> ex)
+{
+    Console.WriteLine(ex.Message);
+}
+
+public record Weather(string State, int Temperature);
